65091794f49fe29966bf674ecc890e5034bb3c09
Rizwanation <m.rizwan07@gmail.com>
2022-02-08 15:11:17 +0500

    committing changes of surefire plugin, testng.xml and test cases

--- src/test/java/TC_001_POST_Request.java
+++ src/test/java/TC_001_POST_Request.java
@@ -14,7 +14,7 @@ public class TC_001_POST_Request {
     {
         //Specify Base URI
 
-        RestAssured.baseURI = "https://restapi.demoqa.com/customer";
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
 
 
         //Create Request Specification
@@ -29,31 +29,31 @@ public class TC_001_POST_Request {
 
         //Create Json Body to be sent as parameter
 
-        JSONObject requestBody = new JSONObject();
+      //  JSONObject requestBody = new JSONObject();
 
-        requestBody.put("FirstName","John");
-        requestBody.put("LastName","Doe");
-        requestBody.put("UserName","JDoe66");
-        requestBody.put("Password","P@ssW0rd");
-        requestBody.put("Email","JDoe66@test.com");
+//        requestBody.put("FirstName","John");
+//        requestBody.put("LastName","Doe");
+//        requestBody.put("UserName","JDoe66");
+//        requestBody.put("Password","P@ssW0rd");
+//        requestBody.put("Email","JDoe66@test.com");
 
             //convert created body to JSONString so it can be accepted as a String parameter
 
-        httpRequest.body(requestBody.toJSONString());
+//        httpRequest.body(requestBody.toJSONString());
 
 
 
 
         //Create Response Object
 
-        Response response = httpRequest.request(Method.POST,"/register");
+        Response response = httpRequest.request(Method.GET,"posts/1");
 
         String ResponseBody  = response.getBody().asString();
 
         //Validate Headers
 
         int statusCode = response.getStatusCode();
-        Assert.assertEquals(statusCode,201);
+        Assert.assertEquals(statusCode,200);
 
         //Validate Body
 
