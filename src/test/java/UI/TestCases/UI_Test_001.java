f72f12cafc610be0a02d7c879bbb77ba9bc753b8
Rizwanation <m.rizwan07@gmail.com>
2022-02-10 15:48:07 +0500

    after name change

---
+++ src/test/java/UI/TestCases/UI_Test_001.java
@@ -0,0 +1,33 @@
+package UI.TestCases;
+
+import groovy.util.MapEntry;
+import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidArgumentException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+
+public class UI_Test_001 extends base {
+
+
+    @Test
+    public void UI_Test_01_SwitchWindows() {
+        
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
